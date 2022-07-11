0f7e82743ed9c39166993b0e360a7e552e5fb8aa
Andres Felipe Galeano Alarcon <andres.galeano@symplifica.com>
2022-07-11 15:30:50 -0500

    Modify redirect for authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+    if user_signed_in? && current_user.profile != nil
+      root_path
+    else
+      new_profile_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    # return the path based on resource
+    user_session_path
+  end
+
+  def after_sign_up_path_for(resource)
+    new_profile_path
+  end
 end
