5caafa4c538eb26d8891cfc17a10d2514ef60099
Andres Felipe Galeano Alarcon <andres.galeano@symplifica.com>
2022-07-14 13:48:43 -0500

    Modify feature like and follow

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,15 @@ class Post < ApplicationRecord
            source_type: Post.name
 
 
-  def like?(current_profile, follow_profile)
+  def like?(current_profile, post_id)
     !!Profile.find(current_profile).profile_linkables.find do |follow|
-      follow.kind == "like" && follow.profile_id == current_profile && follow.profile_linkable_external.profile_id == follow_profile
+      follow.kind == "like" && follow.profile_id == current_profile && follow.profile_linkable_external_id == post_id
     end
+  end
+
+  def count_like(post_id)
+    ProfileLinkable.select do |follow|
+      follow.kind == "like" && follow.profile_linkable_external.id == post_id
+    end.count
   end
 end
